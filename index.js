1479f5f9b31fda23b1a088d38ee4f652f30ebfd9
rickmoralis <rickmoralis@rickmoraliss-iMac.local>
2024-08-13 00:46:13 +0300

    fix index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bot = new TelegramBot(token, {polling: true});
 const webAppUrl = 'https://1win-global-pro.com/bonus500';
 const imgPATH = "https://liquipedia.net/commons/images/thumb/2/24/1win_2024_lightmode.png/600px-1win_2024_lightmode.png"
 const dbPath = "https://sheetdb.io/api/v1/9r9b3flxo3dvn"
+
 const saveUser = async (user) => {
     try {
         const response = await fetch(dbPath, {
@@ -36,10 +37,7 @@ const validateUser = async (id) => {
         if (!response.ok) {
             throw new Error(`Ошибка при получении данных пользователя: ${response.statusText}`);
         }
-
         const userData = await response.json();
-        console.log(userData);
-
 
         if (!userData.length) {
             return false;
@@ -53,14 +51,17 @@ const validateUser = async (id) => {
 }
 
 
-const sendNotification = async (chatId, message) => {
-    try {
-        await bot.sendMessage(chatId, message);
-        console.log(`Сообщение отправлено пользователю с chatId: ${chatId}`);
-    } catch (error) {
-        console.error('Ошибка при отправке уведомления:', error);
-    }
-};
+bot.onText(/\/echo (.+)/, (msg, match) => {
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+
+    const chatId = msg.chat.id;
+    const resp = match[1]; // the captured "whatever"
+
+    // send back the matched "whatever" to the chat
+    bot.sendMessage(chatId, resp);
+});
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text
@@ -85,20 +86,22 @@ bot.on('message', async (msg) => {
 
     if (text === '/start') {
         await bot.sendPhoto(chatId, imgPATH, {
-            caption: 'ВЫБЕРИТЕ ТИП ИГРЫ',
+            caption: 'Этот бот - полная замена официального сайта 1win в России и странах СНГ \n' +
+                '\n' +
+                'Мы вывели казино на новый уровень, теперь можно играть в любимые слоты прямо в телеграмме 🎰\n' +
+                '\n' +
+                'Нажимайте на кнопку Регистрация и получите Бонус + 500% к депозиту и 30% кэшбэк на казино 💸',
             reply_markup: {
-                keyboard: [
-                    [{text: '🎮 ИГРАТЬ И ВЫИГРЫВАТЬ', web_app: {url: webAppUrl}}]
+                inline_keyboard: [
+                    [{text: 'Что умеет этот бот? 🤖', web_app: {url: webAppUrl}}],
+                    [{text: 'Акции и Бонусы ✈️', web_app: {url: webAppUrl}}],
+                    [{text: 'Регистрация с Промокодом 🚀', web_app: {url: webAppUrl}}],
+                    [{text: 'Подписаться на канал 1win Premium 😍', web_app: {url: webAppUrl}}],
                 ],
                 resize_keyboard: true,
                 one_time_keyboard: true,
             }
         });
-        setTimeout(() => {
-            sendNotification(chatId, 'Напоминание о важном событии!');
-        }, 60000);
-
-        console.log(`Пользователь ${firstName} ${languageCode} (${username || 'без имени пользователя'}) с ID ${userId} нажал /start.`);
     }
-
 });
+
